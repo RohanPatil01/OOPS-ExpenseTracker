050ec95823d21a3d1edefa0697d8077f3c9afb4b
Rohan <rohanjpatil63@gmail.com>
2024-04-28 15:12:28 +0530

    Added optimize_settlement

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -21,6 +21,7 @@ class ExpenseTracker:
         self.__record_paid_by()
         self.__record_balance_sheet()
         self.__record_settlement()
+        self.__optimize_settlement()
         
     def __str__(self):
         return "Transaction recorded successfully!"
@@ -72,14 +73,50 @@ class ExpenseTracker:
             if payer != receiver:
                 amount_owed = group['amount'].sum()
                 settlement_data.append({'Name': payer, "Who'll pay": receiver, 'Amount': amount_owed})
-        
+                
         self.settlement_df = pd.DataFrame(settlement_data)
+        
+
+    def __optimize_settlement(self):
+        amount = list(self.settlement_df["Amount"])
+        payers = list(self.settlement_df["Name"])
+        receivers = list(self.settlement_df["Who'll pay"])
+    
+        pairs = []
+        for i, payer in enumerate(payers):
+            for j, receiver in enumerate(receivers):
+                if i == j:
+                    pairs.append([payer, receiver])
+    
+        similar_pairs_indices = []
+        for i, pair1 in enumerate(pairs):
+            for j, pair2 in enumerate(pairs):
+                if i < j and set(pair1) == set(pair2):
+                    similar_pairs_indices.append((i, j))
+    
+        for i in similar_pairs_indices:
+            if amount[i[0]] < amount[i[1]]:
+                amount[i[1]] -= amount[i[0]]
+                amount[i[0]] = 0
+            elif amount[i[0]] == amount[i[1]]:
+                amount[i[0]] = 0
+                amount[i[1]] = 0
+            else:
+                amount[i[0]] -= amount[i[1]]
+                amount[i[1]] = 0
+    
+        optimized_settlement_data = ({'Name': payers, "Who'll pay": receivers, 'Amount': amount})
+  
+    
+        self.optimized_settlement_df = pd.DataFrame(optimized_settlement_data)
 
         with pd.ExcelWriter('ExpenseTracker.xlsx') as writer:
             self.final_transactions_df.to_excel(writer, sheet_name='Transactions', index=False)
             self.final_paid_by_df.to_excel(writer, sheet_name='PaidBy', index=False)
             self.balance_sheet_df.to_excel(writer, sheet_name='BalanceSheet', index=True)
             self.settlement_df.to_excel(writer, sheet_name='Settlement', index=False)
+            self.optimized_settlement_df.to_excel(writer, sheet_name='OptimizedSettlement', index=False)
+    
 
     def show_transaction(self):
         return self.current_transactions_df
